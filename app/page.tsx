9c327ac20f1ea37a72a9094419623355e088d985
SamuelZ12 <zhangsamuel12@gmail.com>
2025-01-12 13:16:33 -0500

    Update profile pic outline

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <Image
           src="/headshot.png"
           alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5"
+          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 ring-2 ring-neutral-200 dark:ring-neutral-800 transition-colors duration-200"
           unoptimized
           width={160}
           height={160}
