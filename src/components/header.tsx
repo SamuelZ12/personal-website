60e65686b2ac899ca14b3bb5bad5049e9d482a10
SamuelZ12 <zhangsamuel12@gmail.com>
2026-03-29 16:00:59 -0400

    remove green circle

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,11 +15,7 @@ const Header = () => {
                         {CONFIG.title}
                     </p>
                 )}
-                <p className='flex items-center gap-2 text-sm text-muted-foreground/80'>
-                    <span className='relative flex h-2 w-2'>
-                        <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75' />
-                        <span className='relative inline-flex h-2 w-2 rounded-full bg-green-500' />
-                    </span>
+                <p className='text-sm text-muted-foreground/80'>
                     Toronto, Canada
                 </p>
             </div>
